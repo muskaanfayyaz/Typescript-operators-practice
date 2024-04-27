b8278a668050267394868b2249e3cda9abd2e9e8
muskaan fayyaz <muskaanfayyaz3@gmail.com>
2024-04-27 12:13:04 +0500

    operators

---
+++ main.ts
@@ -0,0 +1,48 @@
+let a: number = 10;
+let b: number = 5;
+
+// Arithmetic operators
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Increment:", a++);
+console.log("Decrement:", b--);
+console.log("\n");
+
+
+// Assignment operators
+let assign: number = a;
+console.log( "addition assignment operator", assign += b);
+console.log( "subtraction assignment operator", assign -= b);
+console.log( "multiplication assignment operator", assign *= b);
+console.log( " division assignment operator", assign /= b);
+console.log( "modulus assignment operator", assign %= b);
+console.log( "\n");
+
+
+
+// Comparison operators
+console.log("Is Equal:", a === b);
+console.log("Is Not Equal:", a !== b);
+console.log("Is Greater:", a > b);
+console.log("Is Less:", a < b);
+console.log("Is Greater Or Equal:", a >= b);
+console.log("Is Less Or Equal:", a <= b);
+console.log("\n");
+
+
+// Logical operators
+console.log("And Operator:", (a > 0) && (b > 0));
+console.log("Or Operator:", (a > 0) || (b > 0));
+console.log("Not Operator:", !(a > 0));
+console.log("\n");
+
+// Conditional operator
+console.log("Conditional Operator:", (a > b) ? a : b);
+
+
+
+
+
